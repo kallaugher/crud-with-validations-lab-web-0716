55fc2ccbc95dcf35f09919361120f41fb79fc1bf
kallaugher <alicekallaugher@gmail.com>
2016-08-21 15:00:41 -0400

    complete lab

---
+++ app/models/song.rb
@@ -0,0 +1,5 @@
+class Song < ActiveRecord::Base
+  validates :title, presence: true
+  validates :release_year, presence: true, if: 'released', numericality: { less_than_or_equal_to: Date.today.year }
+  validates :title, uniqueness: { scope: :artist_name}
+end
